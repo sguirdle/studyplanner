43038b9bfcef306c92d8eef973488109e60f1f16
sguirdle <foojonah31@gmail.com>
2026-02-24 21:50:30 +0800

    fixed todo navbar

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -45,5 +45,20 @@ function Todo() {
     </div>
   );
 }
+const styles = {
+
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",      // 100% of viewport height
+    width: "100vw",       // 100% of viewport width
+    fontFamily: "Arial, sans-serif",
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+} 
 
 export default Todo;
